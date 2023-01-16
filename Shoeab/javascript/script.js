04a64178e88fb8a9773eeedc5f181ac5d0aa046c
ShoeabPatel <shoeab.patel@brainvire.com>
2023-01-16 19:00:46 +0530

    weekly task

---
+++ Shoeab/javascript/script.js
@@ -0,0 +1,20 @@
+// let js='amazing';
+// if(js==='amazing')
+// {
+//     alert('js is amazing');
+// }
+//datatypes
+let name='shoeab';
+let age=23;
+let amazing=true;
+let children;
+let num=null;
+console.log(name);
+console.log(typeof(name));
+console.log(age);
+console.log(typeof(age));
+console.log(amazing);
+console.log(typeof(amazing));
+console.log(children);
+console.log(num);
+console.log(typeof(num));
