41edae6fd2da4dfaf2fe08c94191cf2d9c7dc491
Amit_Chaudhary <amit.chaudhary@brainvire.com>
2023-01-20 18:57:59 +0530

    Data Operatoes

---
+++ Amit/Javascript/Data Operators/script.js
@@ -0,0 +1,238 @@
+'use strict';
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name1: 'Classico Italiano',
+  location1: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (input1, input2) {
+    return [this.starterMenu[input1], this.starterMenu[input2]];
+  },
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+//Array Destructing
+// const num = [1, 2, 3, 4, 5];
+
+// const [x, y, z] = num;
+// console.log(x, y, z);
+// let [hii, say, kit] = restaurant.mainMenu;
+// console.log(hii, say, kit);
+//switching variables
+
+// [hii, say] = [say, hii];
+// console.log(say, hii);
+
+// let [starter, mainFood] = restaurant.order(2, 3);
+// console.log(starter, mainFood);
+
+// nested destructring
+// const rey = [1, 2, 3, [4, 5, 6]];
+//now if i wanna acces this [4,5,6] i will use array destructing.
+
+// const [one, , , [two, , three]] = rey; //this gaoing means dont destructe this part of the array
+// console.log(one, two, three);
+
+//object destructing
+
+// const { name1, location1, categories } = restaurant;
+// console.log(name1, location1, categories);
+
+// let obj = {
+//   abc1: 'hel0',
+//   gf1: 'hjdfsk',
+// };
+// // let { abc, gf } = obj;
+
+// //we can also give the vairbles new names
+
+// const { abc1: def, gf1: fg } = obj;
+// console.log(def, fg);
+
+//Spread Operator
+
+// const a = [1, 2, 3];
+
+// const b = [4, 5, 6, ...a];
+// console.log(b);
+
+// Rest opreator
+
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(weekdays);
+
+//  const a=[1,2,3,4,5,6,7,8,9];
+// const [a, b, ...hell] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(hell, a, b);
+
+// pizza ingredients
+
+// const pizzaIng = [
+//   prompt('ingrident1'),
+//   prompt('ingrident2'),
+//   prompt('ingrident3'),
+// ];
+// console.log(pizzaIng);
+// const [ing1, ing2, ing3] = pizzaIng;
+// console.log(`this is the pizza made with ${ing1}, ${ing2}and ${ing3}`);
+
+// shortcircuiting using and ,or opreators
+//used for setting  default value
+// using or operator
+
+// console.log(undefined || 0);
+// restaurant.noGuest = 23;
+// const guest2 = restaurant.noGuest || ' no guest available';
+// console.log(guest2);
+
+//And operator will work exactly opposite to the 0r shortcircuiting .
+
+//now suppose if we have restaurant.noGuest=0 so still our output will show the default value so to overcomewt this error we use nullish operator.
+
+//nullish operator
+
+// restaurant.noGuest = 0;
+
+// const guest2 = restaurant.noGuest || 10; this is error or bug for us.
+// const guest2 = restaurant.noGuest ?? 10; //now output is not our default value.
+// console.log(guest2);
+//Default values
+// const [p = 1, q = 1, r = 1] = [8, 9];
+// console.log(p, q, r);
+
+// revision of the above topics
+// using the above topics addition function
+// const add1 = (a, b, ...kl) => {
+//   const input1 = [
+//     prompt('enter num 1'),
+//     prompt('enter num 2'),
+//     prompt('enter num 3'),
+//   ];
+//   const [x = 0, y = 0, z = 0] = input1;
+//   const sum = Number(x) + Number(y) + Number(z);
+//   console.log(sum);
+// };
+// add1(1, 2, 3, 4, 5);
+
+// Coding Challenge #1
+// We're building a football betting app (soccer for my American friends 😅)!
+// Suppose we get data from a web service about a certain game ('game' variable on
+// next page). In this challenge we're gonna work with that data.
+// Your tasks:
+// 1. Create one player array for each team (variables 'players1' and
+// 'players2')
+
+// 7. The team with the lower odd is more likely to win. Print to the console which
+// team is more likely to win, without using an if/else statement or the ternary
+// operator.
+// Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+// Then, call the function again with players from game.scored
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+// stp1
+// const players1 = [game.players[0]];
+// console.log(players1);
+// const players2 = [game.players[1]];
+// console.log(players2);
+const [...players1] = game.players[0];
+console.log(players1);
+const [...players2] = game.players[1];
+console.log(players2);
+
+// stp 2
+//goalkeeper gk
+// 2. The first player in any player array is the goalkeeper and the others are field
+// players. For Bayern Munich (team 1) create one variable ('gk') with the
+// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+// field players
+
+const [gk1, ...fieldPlayers1] = players1;
+// console.log(gk1, fieldPlayers1);
+const [gk2, ...fieldPlayers2] = players2;
+// console.log(gk2, fieldPlayers2);
+// 3. Create an array 'allPlayers' containing all players of both teams (22
+// players)
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+// new array ('players1Final') containing all the original team1 players plus
+// 'Thiago', 'Coutinho' and 'Perisic'
+const players1Final = ['Thiago', 'Coutinho', 'Perisic', ...players1];
+console.log(players1Final);
+// 5. Based on the game.odds object, create one variable for each odd (called
+// 'team1', 'draw' and 'team2')
+const team1 = game.odds.team1;
+const draw = game.odds.x;
+const team2 = game.odds.team2;
+console.log(team1, draw, team2);
+// 6. Write a function ('printGoals') that receives an arbitrary number of player
+// names (not an array) and prints each of them to the console, along with the
+// number of goals that were scored in total (number of player names passed in)
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored`);
+};
+
+printGoals(...game.scored);
+
+// 7. The team with the lower odd is more likely to win. Print to the console which
+// team is more likely to win, without using an if/else statement or the ternary
+// operator.
+team1 < team2 && console.log('team 1 wins');
+team1 > team2 && console.log('team 2 wins');
