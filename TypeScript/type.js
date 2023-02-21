60b4c30c448a340837570e5f74c70e078d0e88ce
ShoeabPatel <shoeab.patel@brainvire.com>
2023-02-21 18:36:10 +0530

    new files

--- TypeScript/type.js
+++
@@ -1,9 +0,0 @@
-var button = document.querySelector('button');
-var number1 = document.getElementById('num1');
-var number2 = document.getElementById('num2');
-function add(num1, num2) {
-    return num1 + num2;
-}
-button.addEventListener('click', function () {
-    console.log(add(+number1.value, +number2.value));
-});
